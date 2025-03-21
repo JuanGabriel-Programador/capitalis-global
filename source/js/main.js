ae95651628f1f07cceded210660560a0c22ea06c
Brenda Romero <160025540+estufita-programa@users.noreply.github.com>
2025-03-21 18:16:46 -0300

    Update main.js

--- source/js/main.js
+++ source/js/main.js
@@ -13,3 +13,12 @@ const showMenu = (toggleId, navId) =>{
 }
 
 showMenu('nav-toggle','nav-menu')
+
+/*=============== MIN Y MAXLENGHT ===============*/
+document.querySelectorAll("input[type='number']").forEach((input) => {
+  input.addEventListener("input", function () {
+    if (this.value.length > this.maxLength) {
+      this.value = this.value.slice(0, this.maxLength);
+    }
+  });
+});
